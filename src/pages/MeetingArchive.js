c7afbe5f2a2afff8204e8676f4ce0caf236faad0
Zayd Ishaque <ZaydIshaque@gmail.com>
2025-04-14 18:55:08 -0400

    Update TSA Hub project

--- src/pages/MeetingArchive.js
+++ src/pages/MeetingArchive.js
@@ -4,7 +4,7 @@ const MeetingArchive = () => {
   return (
     <div className="p-8">
       <h2 className="text-2xl font-bold mb-4">Meeting Archive</h2>
-      <p>Here you'll find summaries or notes from past meetings.</p>
+      <p>Here you'll find summaries or notes from past meetings there are none to my knowledge but I think It would be a nice touch in the future IDK let me know what you think..</p>
     </div>
   );
 };

363d60ac941c228665c49192de6381e4be7926bd
Zayd Ishaque <ZaydIshaque@gmail.com>
2025-10-03 21:26:32 -0400

    update

--- src/firebase.js
+++ src/firebase.js
@@ -5,9 +5,9 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC0ifwEnmEihEjnzKOpjWWW7IRHOv8KyYw",
-  authDomain: "prhs-tsa-hub.firebaseapp.com",
-  projectId: "prhs-tsa-hub",
-  storageBucket: "prhs-tsa-hub.appspot.com", // ✅ fixed typo (.app → .app**spot.com**)
+  authDomain: "",
+  projectId: "",
+  storageBucket: "", // ✅ fixed typo (.app → .app**spot.com**)
   messagingSenderId: "522020225270",
   appId: "1:522020225270:web:cde225b3e8116ce5156cd7",
   measurementId: "G-Z384MG73YW"
